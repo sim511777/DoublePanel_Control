606ec75bb1cf9eb5b177022e20879a4a0317ac36
Sunghyun Shim <sim51177@gmail.com>
2023-09-27 10:18:45 +0900

    초기 커밋.

---
+++ DoublePanel/DoublePanel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+
+namespace DoublePanel {
+    [Designer(typeof(DoublePanel_Designer))]
+    public partial class DoublePanel : UserControl {
+        public DoublePanel() {
+            InitializeComponent();
+        }
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public Panel InnerPanel1 => pnlInner1;
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public Panel InnerPanel2 => pnlInner2;
+    }
+
+    internal class DoublePanel_Designer : ParentControlDesigner {
+        public override void Initialize(System.ComponentModel.IComponent component) {
+            base.Initialize(component);
+
+            if (Control is DoublePanel control) {
+                EnableDesignMode(control.InnerPanel1, "InnerPanel1");
+                EnableDesignMode(control.InnerPanel2, "InnerPanel2");
+            }
+        }
+    }
+}
---
+++ DoublePanel_example/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace DoublePanel_example {
+    partial class Form1 {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent() {
+            this.doublePanel1 = new DoublePanel.DoublePanel();
+            this.doublePanel2 = new DoublePanel.DoublePanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.doublePanel1.InnerPanel1.SuspendLayout();
+            this.doublePanel1.SuspendLayout();
+            this.doublePanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // doublePanel1
+            // 
+            this.doublePanel1.BackColor = System.Drawing.Color.Coral;
+            this.doublePanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            // 
+            // doublePanel1.InnerPanel1
+            // 
+            this.doublePanel1.InnerPanel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.doublePanel1.InnerPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.doublePanel1.InnerPanel1.Controls.Add(this.button1);
+            this.doublePanel1.InnerPanel1.Location = new System.Drawing.Point(23, 50);
+            this.doublePanel1.InnerPanel1.Name = "InnerPanel1";
+            this.doublePanel1.InnerPanel1.Size = new System.Drawing.Size(50, 50);
+            this.doublePanel1.InnerPanel1.TabIndex = 0;
+            // 
+            // doublePanel1.InnerPanel2
+            // 
+            this.doublePanel1.InnerPanel2.BackColor = System.Drawing.Color.Cyan;
+            this.doublePanel1.InnerPanel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.doublePanel1.InnerPanel2.Location = new System.Drawing.Point(126, 42);
+            this.doublePanel1.InnerPanel2.Name = "InnerPanel2";
+            this.doublePanel1.InnerPanel2.Size = new System.Drawing.Size(50, 50);
+            this.doublePanel1.InnerPanel2.TabIndex = 1;
+            this.doublePanel1.Location = new System.Drawing.Point(86, 63);
+            this.doublePanel1.Name = "doublePanel1";
+            this.doublePanel1.Size = new System.Drawing.Size(265, 175);
+            this.doublePanel1.TabIndex = 0;
+            // 
+            // doublePanel2
+            // 
+            this.doublePanel2.BackColor = System.Drawing.Color.Coral;
+            this.doublePanel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            // 
+            // doublePanel2.InnerPanel1
+            // 
+            this.doublePanel2.InnerPanel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.doublePanel2.InnerPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.doublePanel2.InnerPanel1.Location = new System.Drawing.Point(23, 50);
+            this.doublePanel2.InnerPanel1.Name = "InnerPanel1";
+            this.doublePanel2.InnerPanel1.Size = new System.Drawing.Size(50, 50);
+            this.doublePanel2.InnerPanel1.TabIndex = 0;
+            // 
+            // doublePanel2.InnerPanel2
+            // 
+            this.doublePanel2.InnerPanel2.BackColor = System.Drawing.Color.Cyan;
+            this.doublePanel2.InnerPanel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.doublePanel2.InnerPanel2.Location = new System.Drawing.Point(126, 42);
+            this.doublePanel2.InnerPanel2.Name = "InnerPanel2";
+            this.doublePanel2.InnerPanel2.Size = new System.Drawing.Size(50, 50);
+            this.doublePanel2.InnerPanel2.TabIndex = 1;
+            this.doublePanel2.Location = new System.Drawing.Point(386, 63);
+            this.doublePanel2.Name = "doublePanel2";
+            this.doublePanel2.Size = new System.Drawing.Size(265, 175);
+            this.doublePanel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(48, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.doublePanel2);
+            this.Controls.Add(this.doublePanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.doublePanel1.InnerPanel1.ResumeLayout(false);
+            this.doublePanel1.ResumeLayout(false);
+            this.doublePanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DoublePanel.DoublePanel doublePanel1;
+        private DoublePanel.DoublePanel doublePanel2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
